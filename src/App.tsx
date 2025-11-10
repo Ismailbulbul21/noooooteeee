8cc96366ce34beea6322d5cbd05e2de29fea72f0
Ismailbulbul21 <ismailbulbul381@gmail.com>
2025-11-10 17:40:31 +0300

    yes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
 import DataDeletion from './pages/delete'
+import PrivacyPolicy from './pages/privacy'
 import './App.css'
 
 function App() {
-  return <DataDeletion />
+  return (
+    <Routes>
+      <Route path="/" element={<DataDeletion />} />
+      <Route path="/delete-account" element={<DataDeletion />} />
+      <Route path="/privacy" element={<PrivacyPolicy />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  )
 }
 
 export default App
